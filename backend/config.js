ce042536bb8241917abe85a93e2b21b6a17b412e
Jurijs <jurijs.salijevs@gmail.com>
2020-11-15 21:47:04 +0200

    master

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config();
+
 export default {
-    MONGODB_URL: process.env.MONGODB_URL,
+  MONGODB_URL: process.env.MONGODB_URL,
 };
