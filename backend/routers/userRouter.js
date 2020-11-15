7250661589463b967e0422c87bc5fa5d8fc94cb4
Jurijs <jurijs.salijevs@gmail.com>
2020-11-16 00:21:58 +0200

    master

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,24 +1,30 @@
 import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
 import User from '../models/userModel';
+import { generateToken } from '../utils';
 
 const userRouter = express.Router();
 
-userRouter.get('/createadmin', async (req, res) => {
-  try {
-    const user = new User({
-      name: 'admin',
-      email: 'admin@example.com',
-      password: 'jsamazona',
-      isAdmin: true,
-    });
-    const createdUser = await user.save();
-    res.send(createdUser);
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
-
-userRouter.post('/signin', async (req, res) => {
+userRouter.get(
+  '/createadmin',
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const user = new User({
+        name: 'admin',
+        email: 'admin@example.com',
+        password: 'jsamazona',
+        isAdmin: true,
+      });
+      const createdUser = await user.save();
+      res.send(createdUser);
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
+  })
+);
+userRouter.post(
+  '/signin',
+  expressAsyncHandler(async (req, res) => {
     const signinUser = await User.findOne({
       email: req.body.email,
       password: req.body.password,
@@ -26,10 +32,16 @@ userRouter.post('/signin', async (req, res) => {
     if (!signinUser) {
       res.status(401).send({
         message: 'Invalid Email or Password',
-  
       });
-    }    
+    } else {
+      res.send({
+        _id: signinUser._id,
+        name: signinUser.name,
+        email: signinUser.email,
+        isAdmin: signinUser.isAdmin,
+        token: generateToken(signinUser),
+      });
+    }
   })
-
-
+);
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import data from './data';
 import config from './config';
 import userRouter from './routers/userRouter';
 
+
 mongoose
   .connect(config.MONGODB_URL, {
     useNewUrlParser: true,
@@ -32,6 +33,10 @@ app.get('/api/products/:id', (req, res) => {
   } else {
     res.status(404).send({ message: 'Product Not Found!' });
   }
+});
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === 'ValidationError' ? 400 : 500;
+  res.status(status).send({ message: err.message });
 });
 app.listen(5000, () => {
   console.log('serve at http://localhost:5000');
